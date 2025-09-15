5aadb39a67782972cbd45c5f573d2a9da23398bc
YADAVALLI SAI NAGA SRI RAMYA <22a31a0523@pragati.ac.in>
2025-09-15 19:00:56 +0530

    Add solution for Find Closest Person

--- LeetCode/Find-Closest-Person/Solution.py
+++ LeetCode/Find-Closest-Person/Solution.py
@@ -1,11 +1,17 @@
-class Solution:
-    def findClosest(self, x: int, y: int, z: int) -> int:
-        d1 = abs(z - x)
-        d2 = abs(z - y)
-
-        if d1 < d2:
+class Solution(object):
+    def findClosest(self, x, y, z):
+        """
+        :type x: int
+        :type y: int
+        :type z: int
+        :rtype: int
+        """
+        d1=abs(x-z)
+        d2=abs(y-z)
+        if d1<d2:
             return 1
-        elif d1 > d2:
+        elif d1>d2:
             return 2
-        else:
-            return 0
+        return 0
+        
+        
