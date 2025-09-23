2c72c5ddd78ffbbf1f3b35f9fc25f4adcafc472b
YADAVALLI SAI NAGA SRI RAMYA <22a31a0523@pragati.ac.in>
2025-09-23 18:48:54 +0530

    Add solution for Check If It Is a Good Array

---
+++ LeetCode/Check-If-It-Is-a-Good-Array/Solution.py
@@ -0,0 +1,11 @@
+    public boolean isGoodArray(int[] A) {
+        int x = A[0], y;
+        for (int a: A) {
+            while (a > 0) {
+                y = x % a;
+                x = a;
+                a = y;
+            }
+        }
+        return x == 1;
+    }
