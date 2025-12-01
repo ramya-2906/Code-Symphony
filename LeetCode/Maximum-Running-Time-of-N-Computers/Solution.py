ee919c789886764bcae618e96a0c62e9472899e4
YADAVALLI SAI NAGA SRI RAMYA <22a31a0523@pragati.ac.in>
2025-12-01 23:36:36 +0530

    Add solution for Maximum Running Time of N Computers

---
+++ LeetCode/Maximum-Running-Time-of-N-Computers/Solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        batteries.sort()
+        power = sum(batteries)
+        while power // n < batteries[-1]:
+            power -= batteries.pop()
+            n -= 1
+        return power // n
